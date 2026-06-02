35a4d7cbe23910a4b2d12817dae58ad109385871
JFKleyn <101085158+JFKleyn@users.noreply.github.com>
2026-06-02 10:17:08 +0200

    Initial React Setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { HomePage } from "./pages/home/HomePage";
+import { Routes, Route } from "react-router";
+import "./App.css";
+
+function App() {
+  return (
+    <Routes>
+      <Route index element={<HomePage />} />
+    </Routes>
+  );
+}
+
+export default App;
