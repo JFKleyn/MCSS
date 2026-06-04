6a66902e1caaa513d6b34a22179740e9df8179c8
JFKleyn <101085158+JFKleyn@users.noreply.github.com>
2026-06-04 17:11:33 +0200

    Started with creating the Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import './Footer.css';
+
+export function Footer() {
+  return (
+    <>
+      <div className="footer">
+        Hello
+        <p>© 2024 MCSS. All rights reserved.</p>
+      </div>
+    </>
+  );
+}
+
+/* <p>© 2024 MCSS. All rights reserved.</p> */
--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,10 +1,12 @@
 import { Header } from "../../components/Header";
+import { Footer } from "../../components/Footer";
 import "./HomePage.css";
 
 export function HomePage() {
   return (
     <>
       <Header />
+      <Footer />
     </>
   );
 }
