41054c60b269d71232c1c3f548f3434daf63db88
JFKleyn <101085158+JFKleyn@users.noreply.github.com>
2026-06-10 14:36:17 +0200

    Created and completed the Why Choose Us section

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,11 +1,13 @@
 import { Footer } from "../../components/Footer";
 import { HeroSection } from "./Hero"
+import { WhyChooseUs } from "./WhyChooseUs";
 import "./HomePage.css";
 
 export function HomePage() {
   return (
     <>
       <HeroSection />
+      <WhyChooseUs />
       <Footer />
     </>
   );
