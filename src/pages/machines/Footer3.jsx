1df1786db338f5e4523fd514b4d3288291b89852
JFKleyn <101085158+JFKleyn@users.noreply.github.com>
2026-06-19 11:08:16 +0200

    Created and completed Footer3 for the machine page

---
+++ src/pages/machines/Footer3.jsx
@@ -0,0 +1,18 @@
+import "../about/Footer2.css";
+
+export function Footer3(){
+  return(
+    <div className="footer2-section">
+      <h2>WE SOURCE SPECIALIZED EQUIPMENT</h2>
+      <p>Our network spans South Africa and neighboring countries. If you need specific machinery not listed, we'll locate it for you.</p>
+      <div className="footer2-buttons">
+        <button className="footer2-btn1">
+          <h5>CONTACT US NOW</h5>
+        </button>
+        <button className="footer2-btn2">
+          <h5>REQUEST A QUOTE</h5>
+        </button>
+      </div>
+    </div>
+  )
+}
--- src/pages/machines/MachinePage.jsx
+++ src/pages/machines/MachinePage.jsx
@@ -1,4 +1,5 @@
 import { Footer } from "../../components/Footer";
+import { Footer3 } from "./Footer3";
 import { MachineHero } from "./MachineHero"
 
 
@@ -6,6 +7,7 @@ export function MachinePage() {
   return (
     <>
       <MachineHero />
+      <Footer3 />
       <Footer />
     </>
   );
