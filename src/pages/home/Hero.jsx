b29487747c713d5abd5288f5136e164b32e3a2aa
JFKleyn <101085158+JFKleyn@users.noreply.github.com>
2026-07-02 16:24:44 +0200

    Round 1 of testing the contact form

--- src/pages/home/Hero.jsx
+++ src/pages/home/Hero.jsx
@@ -1,5 +1,5 @@
 import { Header } from "../../components/Header";
-import { Link, NavLink } from "react-router";
+import { Link } from "react-router";
 import Hero from "../../assets/hero-back.webp"
 import leadLogo from "../../assets/Lead.webp"
 import wdhearnLogo from "../../assets/WDHearn.webp"
